c8d449f3bbb7efba2284932a9390eb17006b0b87
Tony <10666779@qq.com>
2017-01-10 18:30:21 +0800

    df
    df

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,4 +1,21 @@
 Page({
+   data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    current: 0,
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  changeSwiper: function(event) {
+    this.setData({
+      current: event.detail.current
+    });
+  },
   onLoad: function(options) {
     this.setData({
       id: options.id
