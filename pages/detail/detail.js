728feb262817bc8d65e03e7f9be1e94e7edb172f
sunylt <Tony@Macbook-Pro.local>
2017-01-11 00:33:46 +0800

    专题页跟详情页
    专题页跟详情页

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,21 +1,35 @@
 Page({
    data: {
     imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      '/image/img-demo-1.png',
+      '/image/img-demo-1.png',
+      '/image/img-demo-1.png'
     ],
     current: 0,
     indicatorDots: true,
     autoplay: false,
     interval: 5000,
-    duration: 1000
+    duration: 1000,
+    amount: 1
   },
   changeSwiper: function(event) {
     this.setData({
       current: event.detail.current
     });
   },
+
+  reduceNumber: function() {
+    let amount = this.data.amount;
+    amount > 1 && this.setData({'amount': amount-1});
+  },
+
+  addNumber: function() {
+    let amount = this.data.amount + 1;
+    this.setData({
+      amount: amount
+    });
+  },
+
   onLoad: function(options) {
     this.setData({
       id: options.id
