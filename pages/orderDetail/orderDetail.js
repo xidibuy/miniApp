30b2d56f6c2428ab42f601ed395155c2f26642d8
yangyang <mayangyang@gongchang.com>
2017-02-20 08:08:33 +0800

    订单详情页
    订单详情页

--- pages/orderDetail/orderDetail.js
+++ pages/orderDetail/orderDetail.js
@@ -1,19 +1,26 @@
 // pages/orderDetail/orderDetail.js
 Page({
-  data:{},
+  data:{
+    second:3*60*60
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+    // countdown(this);
   },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
+  countdown: function(that){
+    let second = that.data.second;
+    if(second == 0){
+      that.setData({
+        second:"time out"
+      });
+      return
+    }
+    let time = setTimeout(function(){
+      that.setData({
+        second:second-1
+      });
+      countdown(that);
+    },1000);
   }
+
 })
