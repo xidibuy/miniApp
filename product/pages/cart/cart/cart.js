802fbff591df882a695dcd4fcda750eca833968c
朱鹏程 <309579250@qq.com>
2017-02-23 15:40:44 +0800

    修复购物车

--- product/pages/cart/cart/cart.js
+++ product/pages/cart/cart/cart.js
@@ -4,16 +4,16 @@ Page({
     data: {
         img: app.globalData.img,
         dataUrl: app.globalData.data,
-        list: [],
-        invalidGoodsNum: 0
+        list: []
+
     },
 
     onLoad: function () {
         const self = this;
         // 1个商品
-        const listUrl = app.globalData.data + 'cart/normal1.json';
+        // const listUrl = app.globalData.data + 'cart/normal1.json';
         // 6个商品
-        // const listUrl = app.globalData.data + 'cart/normal6.json';
+        const listUrl = app.globalData.data + 'cart/normal6.json';
         // 空购物车
         // const listUrl = app.globalData.data + 'cart/null.json';
         // 获取列表
@@ -27,7 +27,22 @@ Page({
 
         })
     },
-    tapName: function (e) {
-        console.log(e);
-    }
+
+    toggleItemCheckbox: function (e) {
+        let self = this;
+        let currentId = e.currentTarget.dataset.id;
+        let currentState = !!e.detail.value.length;
+        let changeCurrentState = function (item,index) {
+            if (currentId == item.id) {
+                // console.log(item,index)
+                item.checked = currentState
+            }
+            return item;
+
+        }
+        self.setData({
+            list: self.data.list.map(changeCurrentState)
+        })
+    },
+
 });
