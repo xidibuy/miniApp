53d5a490255be337fb81948d567087a682354fbf
yangyang <mayangyang@gongchang.com>
2017-02-25 11:42:13 +0800

    asffs
    asfdsdg

--- product/app.js
+++ product/app.js
@@ -47,7 +47,8 @@ App({
   globalData:{
     userInfo:null,
     // data: "https://wxapp.xidibuy.com"
-    img: "http://172.16.14.96:8888/image/",
-    data: "http://172.16.14.96:8888/data/"
+    img: "http://172.16.14.70:8888/image/",
+    data: "http://172.16.14.70:8888/data/"
+
   }
 })
