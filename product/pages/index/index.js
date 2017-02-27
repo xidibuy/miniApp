3f1c927c8aa9a1d41ad3d6dc3d5a3e8fb19a534e
yangyang <mayangyang@gongchang.com>
2017-02-27 18:48:09 +0800

    本地
    本地

--- product/pages/index/index.js
+++ product/pages/index/index.js
@@ -1,7 +1,7 @@
 //获取应用实例
 var app = getApp();
-// const url = app.globalData.dataRemote;
-const url = app.globalData.data;
+const url = app.globalData.dataRemote;
+// const url = app.globalData.data;
 Page({
   data: {
     current: 0,
@@ -18,8 +18,8 @@ Page({
 
   onLoad: function (options) {
     const _this = this;
-    // const indexUrl = url + '/index/home';
-    const indexUrl = url + '/index.json';
+    const indexUrl = url + '/index/home';
+    // const indexUrl = url + '/index.json';
     app.fetchApi(indexUrl, function (options) {
       _this.setData({
         index: options.data
