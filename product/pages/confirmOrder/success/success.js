cb5ce2492e145f8237b72612067f79046e62d803
yangyang <mayangyang@gongchang.com>
2017-03-06 17:27:13 +0800

    SFSAF

--- product/pages/confirmOrder/success/success.js
+++ product/pages/confirmOrder/success/success.js
@@ -3,6 +3,9 @@ Page({
   data:{},
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+    this.setData({
+      aaa: wx.getStorageSync('aaa')
+    })
   },
   onReady:function(){
     // 页面渲染完成
@@ -15,5 +18,8 @@ Page({
   },
   onUnload:function(){
     // 页面关闭
+  },
+  test(){
+    wx.setStorageSync('aaa', '111')
   }
 })
